80ce7e610c47b1a52bd61f6828ff92f14c540327
Daniel Felizardo <daniel.david772@gmail.com>
2019-11-18 09:01:27 -0300

    refactor: Mobile styles index

---
+++ mobile/src/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { Container } from './styles.index'
+
+export default function App() {
+  return (
+    <Container>
+      <Text>Hello World</Text>
+    </Container>
+  );
+}
